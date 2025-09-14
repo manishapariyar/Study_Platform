52f883d403eebe9121c8e5ed6986872a108a68b1
manishapariyar <manishapariyar196@gmail.com>
2025-09-14 22:41:00 +0545

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import Header from "@/components/ui/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import { dark } from "@clerk/themes";
+
+export const metadata: Metadata = {
+  title: "Acadiora",
+  description: "",
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+  weight: ["400", "700"],
+});
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+    }}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.className}`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>
+                  Made with 💗 by  Manisha
+                </p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ components/ui/Header.tsx
@@ -0,0 +1,91 @@
+
+"use client";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { ChevronDown, FileText, GraduationCap, LayoutDashboard, StarIcon } from 'lucide-react'
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu'
+
+const Header = () => {
+  return (
+    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-xl z-50 supports-[backdrop-filter]:bg-background/60 pb-1' >
+
+      <nav className='container mx-auto flex h-16 items-center justify-between px-4 mt-4 '>
+        <Link href='/'>
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={180} // you can keep this for layout
+            height={40} // adjust height proportional to width
+            className='object-contain '
+          />
+
+        </Link>
+        <div className='flex items-center space-x-2'>
+          <SignedIn>
+            <Link href={'/dashboard'}>
+              <Button>
+                <LayoutDashboard className=' h-4 w-4' />
+                <span className='hidden md:block'>
+                  Dashboard
+                </span>
+              </Button></Link>
+            <DropdownMenu >
+              <DropdownMenuTrigger asChild>
+                <Button>
+                  <StarIcon className='h-4 w-4' />
+                  <span className='hidden md:block'>Growth Tools</span>
+                  <ChevronDown className='h-4 w-4' />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href={'/resume'} className='flex items-center gap-2'>
+                    <FileText className='h-4 w-4' />
+                    <span>Build Resume</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href={'/interview'} className='flex items-center gap-2'>
+                    <GraduationCap className='h-4 w-4' />
+                    <span>Interview Prep</span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button variant="outline">Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  userButtonPopoverCard: 'shadow-xl border',
+                  userPreviewMainIdentifier: 'font-semibold',
+                  avatarBox: 'w-10 h-10',
+                },
+              }}
+              afterSignOutUrl="/"
+            />
+
+          </SignedIn>
+        </div>
+
+
+
+
+      </nav>
+
+
+
+    </header>
+  )
+}
+
+export default Header
